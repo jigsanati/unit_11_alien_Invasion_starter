0eae85f803825ca8e1d3363ae8eba488d6ab5dd6
jigsanati <jigsanati@yahoo.com>
2025-08-06 19:29:09 -0400

    i made some changes on setting

--- settings.py
+++ settings.py
@@ -22,3 +22,19 @@ class Settings:
         self.bullet_w = 25
         self.bullet_h = 80
         self.bullet_amount = 5
+
+        self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
+        self.alien_w = 40
+        self.alien_h = 40
+        self.fleet_speed = 2
+        self.fleet_direction = 1
+        self.fleet_drop_speed = 40
+
+        self.button_w = 200
+        self.button_h = 50
+        self.button_color = (0, 135, 50)
+
+        self.text_color = (255, 255, 255)
+        self.button_font_size = 48
+        self.HUD_FONT_SIZE = 20
+        self.font_file = Path.cwd() / 'Assets' / 'fonts' / 'silkscreen' / 'Silkscreen-Bold.ttf'
